08d23c110021eb12b97398e112d932468e08c149
jason zhang <jzhang20@stuy.edu>
2021-01-14 21:28:32 -0500

    RADIX SORT WORKS O/

--- Radix.java
+++ Radix.java
@@ -4,7 +4,7 @@ public class Radix {
     if (col >= length(n)) {
       return 0;
     }
-    return (int) (n / Math.pow(10, col)) % 10;
+    return (int) Math.abs((n / Math.pow(10, col)) % 10);
   }
 
   public static int length (int n) {
@@ -48,6 +48,25 @@ public class Radix {
   }
 
   public static void radixSort (SortableLinkedList data) {
-
+    SortableLinkedList [] buckets = new SortableLinkedList [19];
+    int sizeBig = 1;
+    int a = 0;
+    for (int i = 0; i < 19; i++) {
+      buckets[i] = new SortableLinkedList();
+    }
+    for (int j = 0; j < sizeBig; j++) {
+      while (data.size() != 0) {
+        a = data.remove(0);
+        if (j == 0 && sizeBig < length(a)) {
+          sizeBig = length(a);
+        }
+        if (a < 0) {
+          buckets[9 - nth(a, j)].add(a);
+        } else {
+          buckets[9 + nth(a, j)].add(a);
+        }
+      }
+      merge(data, buckets);
+    }
   }
 }
