f76c6f6c5c4a7c104c5fd5e5def1e183721f7a8b
jason zhang <jzhang20@stuy.edu>
2021-01-14 23:02:37 -0500

    fixed to make sure smallest integer works

--- Radix.java
+++ Radix.java
@@ -1,6 +1,5 @@
 public class Radix {
   public static int nth (int n, int col) {
-    int exponentVal = 1;
     if (col >= length(n)) {
       return 0;
     }
@@ -8,23 +7,22 @@ public class Radix {
   }
 
   public static int length (int n) {
+    if (n == -2147483648) {
+      n += 1;
+    }
     if (n < 0) {
       n = Math.abs(n);
     }
     if (n == 0) {
       return 1;
     }
-    return (int) Math.log10(n) + 1;
-  }
 
-  public static void main (String [] args) {
-    System.out.println(length(-100));
+    return (int) Math.log10(n) + 1;
   }
 
   public static void merge (SortableLinkedList original, SortableLinkedList [] buckets) {
     for (int i = 0; i < buckets.length; i++) {
       original.extend(buckets[i]);
-      // System.out.println(original);
     }
   }
 
